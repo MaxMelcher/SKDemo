f315f100d4f1bcce0caf86fc5547a5198f7a4d36
Max Melcher <Maximilian.Melcher@melcher.it>
2023-12-08 16:59:54 +0100

    plugin to read blog posts

---
+++ Plugins/BlogPlugin.cs
@@ -0,0 +1,30 @@
+using System.ComponentModel;
+using Microsoft.SemanticKernel;
+
+public class BlogPlugin
+{
+
+    [SKFunction, Description("Get the blog posts")]
+    public async Task<List<BlogPost>> GetBlogPosts()
+    {
+        //get the blog posts from https://melcher.dev/feed/feed.json
+        using HttpClient client = new()
+            {
+                BaseAddress = new Uri("https://melcher.dev")
+            };
+        var post = await client.GetFromJsonAsync<List<BlogPost>>("feed/feed.json");
+        return post;
+    }
+}
+
+public class BlogPost
+    {
+        public string url { get; set; }
+        public string title { get; set; }
+        public DateTime date_published { get; set; }
+        public string description { get; set; }
+        public string content { get; set; }
+        public List<string> tags { get; set; }
+        public List<string> categories { get; set; }
+        public string feature { get; set; }
+    }
